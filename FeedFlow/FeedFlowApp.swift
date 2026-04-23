56e5c29aab7aa9c26d9553913acccd3fb7f62e91
Arturo Rey <arturoreysoto@gmail.com>
2026-04-23 23:59:46 +0200

    Initial Commit

---
+++ FeedFlow/FeedFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedFlowApp.swift
+//  FeedFlow
+//
+//  Created by Arturo Rey Soto on 23/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct FeedFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
